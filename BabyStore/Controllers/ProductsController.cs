23f7b1e84cc5c74905b581797acfd636f67edbab
vonrimS <mr.mytab@gmail.com>
2021-09-30 23:33:40 +0300

    more adv data management

--- BabyStore/Controllers/ProductsController.cs
+++ BabyStore/Controllers/ProductsController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using BabyStore.DAL;
 using BabyStore.Models;
 using BabyStore.ViewModels;
+using PagedList;
 
 namespace BabyStore.Controllers
 {
@@ -20,7 +21,7 @@ namespace BabyStore.Controllers
 
         // GET: Products
         
-        public ActionResult Index(string category, string search)
+        public ActionResult Index(string category, string search, string sortBy, int? page)
         {
 
             //instantiate a new view model
@@ -59,12 +60,39 @@ namespace BabyStore.Controllers
 
             if (!String.IsNullOrEmpty(category))
             {
-                products = products.Where(p => p.Category.Name == category);
+                products = products.Where(p => p.Category.Name == category);                
+                viewModel.Category = category;
             }
+
+
+            //sort the results
+            switch (sortBy)
+            {
+                case "price_lowest":
+                    products = products.OrderBy(p => p.Price);
+                    break;
+                case "price_highest":
+                    products = products.OrderByDescending(p => p.Price);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.Name);
+                    break;
+            }
+
 
             //ViewBag.Category = new SelectList(categories);
 
-            viewModel.Products = products;
+            //viewModel.Products = products;
+            const int PageItems = 3;
+            int currentPage = (page ?? 1);
+            viewModel.Products = products.ToPagedList(currentPage, PageItems);
+            viewModel.SortBy = sortBy;
+
+            viewModel.Sorts = new Dictionary<string, string>
+            {
+            {"Price low to high", "price_lowest" },
+            {"Price high to low", "price_highest" }
+            };
 
             //return View(products.ToList());
             return View(viewModel);
--- BabyStore/ViewModels/ProductIndexViewModel.cs
+++ BabyStore/ViewModels/ProductIndexViewModel.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using PagedList;
 
 namespace BabyStore.ViewModels
 {
     public class ProductIndexViewModel
     {
-        public IQueryable<Product> Products { get; set; }
+        public IPagedList<Product> Products { get; set; }
         public string Search { get; set; }
         public IEnumerable<CategoryWithCount> CatsWithCount { get; set; }
         public string Category { get; set; }
+        public string SortBy { get; set; }
+        public Dictionary<string, string> Sorts { get; set; }
 
         public IEnumerable<SelectListItem> CatFilterItems
         {
